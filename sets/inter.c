1a94fb764fd02be84521005c96680d6501a3ab9d
Guti <gutifer666@gmail.com>
2023-07-05 23:19:48 +0200

    inter ok

--- sets/inter.c
+++ sets/inter.c
@@ -1,44 +1,51 @@
 #include <unistd.h>
 
-int repeat (char c, char *str, int pos)
+int pos_char_in_str(char c, char *str)
+{
+	int pos = 0;
+
+	while(str[pos])
+	{
+		if(str[pos] == c)
+			return pos;
+		pos++;
+	}
+	return pos;
+}
+
+int is_char_in_str(char c, char *str)
 {
 	int i = 0;
 
-	while(str[i] != '0' && i < pos)
+	while(str[i])
 	{
-		if (str[i] == c)
+		if(str[i] == c)
 			return 1;
 		i++;
 	}
 	return 0;
+}
 
+void inter(char *str1, char *str2)
+{
+	int index = 0;
 
+	while(str1[index])
+	{
+		// Si el caracter no se encontró antes         y   se encuentra en el segundo str
+		if(pos_char_in_str(str1[index], str1) >= index && is_char_in_str(str1[index], str2))
+			write(1, &str1[index], 1);
+		index++;
+	}
 }
 
 int main(int arc, char **arv)
 {
-
-	int i = 0;
-	int j = 0;
-	int found = 0;
-
 	if (arc == 3)
 	{
-		while(arv[1][i] != '0')
-		{
-			while(arv[2][j] != '0' && found == 0)
-			{
-				if ((arv[1][i] == arv[2][j]) && !repeat(arv[1][i], arv[1], i))
-				{
-					write(1, &arv[1][i], 1);
-					found = 1;
-				}
-				j++;	
-			}
-			i++;
-			found = 0;
-		}	
+		inter(arv[1], arv[2]);	
 	}
 	write(1, "\n", 1);
 	return 0;
+
 }
