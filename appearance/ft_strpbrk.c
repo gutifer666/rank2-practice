0e51a95df6aa67c438556b8aaa68b587528e26df
Guti <gutifer666@gmail.com>
2023-07-08 17:26:41 +0200

    pbrk ok

--- appearance/ft_strpbrk.c
+++ appearance/ft_strpbrk.c
@@ -1,42 +1,22 @@
-int char_in_str(char c, const char *str)
+#include <stdlib.h>
+
+char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	int i = 0;
+	int	index;
 
-	while(str[i])
+	if (!s1 || !s2)
+		return (NULL);
+	index = 0;
+	while (*s1)
 	{
-		if(str[i] == c)
-			return 1;
-		i++;
-	}
-	return 0;
-}
-
-int pos_in_str(char c, const char *str)
-{
-	int i = 0;
-
-	while(str[i])
-	{
-		if(str[i] == c)
-			return i;
-		i++;
-	}
-	return 0;
-}
-
-char *ft_strpbrk(const char *s1, const char *s2)
-{
-	int i = 0;
-
-	while(s2[i])
-	{
-		if(char_in_str(s2[i], s1))
+		index = 0;
+		while (s2[index])
 		{
-			return *s1[pos_in_str(s2[i], s1)];
+			if (*s1 == s2[index])
+				return ((char *)s1);
+			index++;
 		}
-		else
-			i++;
+		s1++;
 	}
-	return &s1[0];
-
+	return (NULL);
 }
