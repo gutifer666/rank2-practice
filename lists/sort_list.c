cf924b9c7d0ec2fc38c5a352f96fee4387df0f24
Guti <gutifer666@gmail.com>
2023-07-17 23:01:44 +0200

    sort list added

--- lists/sort_list.c
+++ lists/sort_list.c
@@ -1,4 +1,23 @@
+#include "ft_list.h"
+
 t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 {
-	
+	int		aux;
+	t_list	*node;
+
+	node = lst;
+	while (lst->next != NULL)
+	{
+		if (((*cmp)(lst->data, lst->next->data)) == 0)
+		{
+			aux = lst->data;
+			lst->data = lst->next->data;
+			lst->next->data = aux;
+			lst = node;
+		}
+		else
+			lst = lst->next;
+	}
+	lst = node;
+	return (lst);
 }
