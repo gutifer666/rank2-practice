985e04f5708c96a451f951983a545e7739fffa55
Guti <gutifer666@gmail.com>
2023-07-29 09:15:26 +0200

    alpha mirror

--- formula/alpha_mirror.c
+++ formula/alpha_mirror.c
@@ -1,39 +1,57 @@
 #include <unistd.h>
 
-void alpha_mirror(char *str)
+int	is_down_case(char c)
+{
+	if(c >= 'a' && c <= 'z')
+		return 1;
+	return 0;
+}
+
+int	is_up_case(char c)
+{
+	if(c >= 'A' && c <= 'Z')
+		return 1;
+	return  0;
+}
+
+int	is_alpha(char c)
+{
+	if(is_down_case(c) || is_up_case(c))
+		return 1;
+	return 0;
+}
+
+void	put_char(char c)
+{
+	write(1, &c, 1);
+}
+
+void	put_opposite(char c)
+{
+	if(is_down_case(c))
+		put_char('a' + 'z' - c);
+	if(is_up_case(c))
+		put_char('A' + 'Z' - c);
+}
+
+void	alpha_mirror(char *str)
 {
 	int i = 0;
-	
-	while(str)
+
+	while(str[i])
 	{
-		if(str[i] >= 65 && str[i]<= 90)
-		{
-			if(str[i] < 'N')
-				write(1, &('A' + 'Z' - str[i]), 1);
-			if(str[i] > 'M')
-				write(1, &('A' - 'Z' + str[i]), 1);
-		}
-		else if(str[i] >= 97 && str[i] <= 122)
-		{
-			
-			if(str[i] < 'n')
-				write(1, str[i] + 'z' -13, 1);
-			if(str[i] > 'm')
-				write(1, str[i] - 'z' + 13);
-		}
+		if(is_alpha(str[i]))
+			put_opposite(str[i]);
 		else
-			write(1, str[i], 1);
+			put_char(str[i]);
 		i++;
 	}
 }
 
-int main(int arc, char **arv)
+int	main(int arc, char **arv)
 {
-
 	if(arc == 2)
-	{
-		alpha_mirror(*arv[1]);
-	}
+		alpha_mirror(arv[1]);
 	write(1, "\n", 1);
-	return 0;
+	return (0);	
 }
